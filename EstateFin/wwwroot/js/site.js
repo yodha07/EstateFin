31c83d29fc515d8c95227353d8b92306d6822506
yodha07 <sutaraditya15@gmail.com>
2025-05-28 18:48:52 +0530

    Cfirst commit

--- EstateFin/wwwroot/js/site.js
+++ EstateFin/wwwroot/js/site.js
@@ -2,3 +2,9 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+//"GoogleKeys": {
+//    "ClientId": "390619856453-mh7g5fj0vm4v60mvihrf52ldk6q5qucb.apps.googleusercontent.com",
+//    "ClientSecret": "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"
+//  },
